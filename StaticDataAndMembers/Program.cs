055d309ce03cee0373deba59f0825be408ab91a9
Covali Andrei <krab4ek@gmail.com>
2022-06-27 17:18:51 +0300

    Add project files.

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,12 @@
+﻿using StaticDataAndMembers;
+
+Console.WriteLine("***** Fun with Static Data *****\n");
+SavingAccount s1 = new SavingAccount(50);
+SavingAccount s2 = new SavingAccount(100);
+SavingAccount s3 = new SavingAccount(1000.75);
+
+
+
+
+
+Console.ReadLine();
---
+++ StaticDataAndMembers/SavingAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace StaticDataAndMembers
+{
+    internal class SavingAccount
+    {
+        public double currBalance;
+        public static double currInterestRate = 0.04;
+        public SavingAccount(double balance)
+        {
+            currBalance = balance;
+        }
+    }
+}
